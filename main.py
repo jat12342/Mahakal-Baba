3a27d4245192b150a90c903b59750777eb494546
jat12342 <bhavishyarajput75@gmail.com>
2024-09-29 12:24:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,222 +1,332 @@
 from kivymd.app import MDApp
-from kivy.uix.screenmanager import Screen,ScreenManager
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen,ScreenManager
 from kivymd.toast.kivytoast.kivytoast import toast
 import requests
 import webbrowser
+from kivymd.uix.label import MDLabel
+from kivymd.uix.card import MDCard
+from kivy.core.audio import SoundLoader
+from kivy.uix.image import Image,AsyncImage
 kv='''
 Manager:
     Fir:
     Sec:
-
+                
 <Fir>:
-    name:'home'
-    id:screen1
-    MDTextField:
-        id:t1
-        hint_text:'ENTER CITY'
-        size_hint_x:1
-        pos_hint:{'center_x':0.5,'center_y':0.8}
-        text_color_normal: 0,0,0,1
-      
+    name:'s1'
+    id:sc1
+    MDTextFieldRect: 
+        id:tf1    
+        size_hint:1,0.1
+        pos_hint:{'center_y':0.83}
+        multiline:True
+        input_filter:'float'
+        
+        
         
     MDFloatingActionButton:
-        id:fb1
-        icon:'magnify'       
-        pos_hint:{'center_x':0.5,'center_y':0.7}
-        size_hint_x:1
+        id:b1
+        pos_hint:{'center_x':0.1,'center_y':0.7}
         on_press:
-            app.wh()
+            app.add('1')            
+        MDTextButton:
+            text:'1'
+            size_hint:0,0     
+           
+    MDFloatingActionButton:
+        id:b2
+        pos_hint:{'center_x':0.3,'center_y':0.7}
+        on_press:
+            app.add('2')
+        MDTextButton:
+            text:'2'  
+            size_hint:0,0                    
+            
+    MDFloatingActionButton:
+        id:b3
+        pos_hint:{'center_x':0.5,'center_y':0.7}
+        on_press:
+            app.add('3')  
+        MDTextButton:
+            text:'3'    
+            size_hint:0,0          
+        
+    MDFloatingActionButton:
+        id:b4
+        pos_hint:{'center_x':0.7,'center_y':0.7}
+        on_press:
+            app.add('4')            
+        MDTextButton:
+            text:'4' 
+            size_hint:0,0             
+        
+    MDFloatingActionButton:
+        id:b÷
+        pos_hint:{'center_x':0.9,'center_y':0.7}
+        md_bg_color:0,0,1,1
+        on_press:
+            app.add('/')           
+        MDTextButton:
+            text:'÷'      
+            size_hint:0,0           
+            color:1,1,1,1
     
-    
-    
-
-    
-    MDLabel:
-        id:l1
-        text:'WHEATHER STATUS'
+    MDFloatingActionButton:
+        id:b5
+        pos_hint:{'center_x':0.1,'center_y':0.6}
+        on_press:
+            app.add('5')            
+        MDTextButton:
+            text:'5'  
+            size_hint:0,0
+             
+    MDFloatingActionButton:
+        id:b6
         pos_hint:{'center_x':0.3,'center_y':0.6}
-        size_hint_x:0.5
-        bold:True
-        
-                                           
-    MDLabel:
-        id:l2
-        text:'TEMPERATURE'
+        on_press:
+            app.add('6')            
+        MDTextButton:
+            text:'6'          
+            size_hint:0,0
+                     
+    MDFloatingActionButton:
+        id:bc
+        pos_hint:{'center_x':0.6,'center_y':0.6}
+        size_hint:0.3,0.07     
+        md_bg_color:0,1,0,1
+        on_press:
+            app.remove()
+        MDTextButton:
+            text:'C'               
+            color:1,0,0,1        
+            size_hint:0,0
+            
+    MDFloatingActionButton:
+        id:bX
+        pos_hint:{'center_x':0.9,'center_y':0.6}
+        md_bg_color:0,0,1,1
+        on_press:
+            app.add('*')           
+        MDTextButton:
+            text:'X'      
+            size_hint:0,0           
+            color:1,1,1,1                        
+
+    MDFloatingActionButton:
+        id:b7
+        pos_hint:{'center_x':0.1,'center_y':0.5}
+        on_press:
+            app.add('7')            
+        MDTextButton:
+            text:'7'          
+            size_hint:0,0
+
+    MDFloatingActionButton:
+        id:b8
         pos_hint:{'center_x':0.3,'center_y':0.5}
-        size_hint_x:0.5   
-        bold:True
-        
-    MDLabel:
-        id:l3
-        text:'PRESSURE'
-        pos_hint:{'center_x':0.3,'center_y':0.4}
-        size_hint_x:0.5   
-        bold:True                               
-                    
-    MDLabel:
-        id:l4
-        text:'WIND SPEED'
-        pos_hint:{'center_x':0.3,'center_y':0.3}
-        size_hint_x:0.5   
-        bold:True                               
-    MDLabel:
-        id:l5
-        text:'DESCRIPTION'
-        pos_hint:{'center_x':0.3,'center_y':0.2}
-        size_hint_x:0.5   
-        bold:True    
-        
-    MDLabel:
-        id:al1
-        text:'---------------'
-        pos_hint:{'center_x':0.8,'center_y':0.6}
-        size_hint_x:0.5   
-        bold:True                                    
-                                      
-    MDLabel:
-        id:al2
-        text:'---------------'
-        pos_hint:{'center_x':0.8,'center_y':0.5}
-        size_hint_x:0.5   
-        bold:True            
+        on_press:
+            app.add('8')            
+        MDTextButton:
+            text:'8'          
+            size_hint:0,0
+            
+    MDFloatingActionButton:
+        id:b9
+        pos_hint:{'center_x':0.5,'center_y':0.5}
+        on_press:
+            app.add('9')            
+        MDTextButton:
+            text:'9'          
+            size_hint:0,0
+            
+    MDFloatingActionButton:
+        id:b.
+        pos_hint:{'center_x':0.7,'center_y':0.5}
+        md_bg_color:0,1,0,1
+        on_press:
+            app.add('.')            
+        MDIconButton:
+            icon:'circle'          
+            size_hint:0,0
+            color:1,0,0,1
 
-    MDLabel:
-        id:al3
-        text:'---------------'
-        pos_hint:{'center_x':0.8,'center_y':0.4}
-        size_hint_x:0.5   
-        bold:True          
+            
+    MDFloatingActionButton:
+        id:b+
+        pos_hint:{'center_x':0.9,'center_y':0.5}
+        md_bg_color:0,0,1,1
+        on_press:
+            app.add('+')           
+        MDTextButton:
+            text:'+'      
+            size_hint:0,0           
+            color:1,1,1,1   
 
-  
+    MDFloatingActionButton:
+        id:b-
+        pos_hint:{'center_x':0.9,'center_y':0.4}
+        md_bg_color:0,0,1,1
+        on_press:
+            app.add('-')           
+        MDTextButton:
+            text:'-'      
+            size_hint:0,0           
+            color:1,1,1,1   
 
-    MDLabel:
-        id:al4
-        text:'---------------'
-        pos_hint:{'center_x':0.8,'center_y':0.3}
-        size_hint_x:0.5   
-        bold:True          
+    MDFloatingActionButton:
+        id:b0
+        pos_hint:{'center_x':0.1,'center_y':0.4}
+        on_press:
+            app.add('0')  
+        MDTextButton:
+            text:'0'          
+            size_hint:0,0
+            
+    MDFloatingActionButton:
+        id:bks
+        pos_hint:{'center_x':0.7,'center_y':0.3}
+        size_hint:0.5,0.07     
+        md_bg_color:0,1,0,1
+        on_press:
+            app.bk()
+        MDIconButton:
+            icon:'keyboard-backspace'               
+            color:1,0,0,1        
+            size_hint:0,0            
 
-    MDLabel:
-        id:al5
-        text:'---------------'
-        pos_hint:{'center_x':0.8,'center_y':0.2}
-        size_hint_x:0.5   
-        bold:True   
-        
+    MDFloatingActionButton:
+        id:sol
+        pos_hint:{'center_x':0.5,'center_y':0.4}
+        size_hint:0.6,0.07     
+        md_bg_color:1,0,0,1
+        on_press:
+            app.solve()
+        MDTextButton:
+            text:'='               
+            color:1,1,1,1       
+            size_hint:0,0            
+
+    MDFloatingActionButton:
+        id:bh
+        pos_hint:{'center_x':0.2,'center_y':0.3}
+        size_hint:0.3,0.07
+        on_press:
+            app.thank()  
+        MDTextButton:
+            text:'HELLO'          
+            size_hint:0,0 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     MDTopAppBar:
-        id:mt1
-        title:'WHEATHER CHECKER'
+        md_bg_color:1,0,0,1
+        size_hint:1,0.1
         pos_hint:{'top':1}
-        left_action_items:[['menu',lambda x:n1.set_state('open')]]                       
-                      
-                                    
+        title:'CALCULATOR'
+        left_action_items:[['menu',lambda x: nd1.set_state('open')]]        
+        right_action_items:[['calculator',lambda x: app.solve()]]
+        
 
+                
+
+                
+
+
+                                                                           
+   
     MDNavigationDrawer:
-        id:n1
-        MDList:
-            pos_hint:{'top':1}
-            OneLineIconListItem:
-                text:'YOUTUBE'
-                on_press:
-                    app.yc()                  
-                IconLeftWidget:
-                    icon:'youtube'
-                    md_bg_color:'red'
-                    on_press:
-                        app.yv()
-                   
-            OneLineIconListItem:
-                text:'CALCULATOR'
-                on_press:
-                    app.ap()
-                IconLeftWidget:
-                    icon:'android'           
-                    md_bg_color:'red'        
-                    on_press:
-                        app.ap()
+        id:nd1  
 
-               
-                      
-                             
-                                           
 
+                                                        
+
+<Sec>:
+    name:'s2'
+    id:sc2     
+       
+        
+        
+        
+        
 '''
 
-
-class Fir(Screen):
-    pass
-    
-    
-class Sec(Screen):
-    pass
-    
-    
 class Manager(ScreenManager):
     pass
 
+class Fir(Screen):
+    pass
 
-class Demo(MDApp):
+class Sec(Screen):
+    pass
+
+
+
+
+
+
+class de(MDApp):
     def build(self):
-        self.u=Builder.load_string(kv)
-        return self.u
-
-    def wh(self):
-        city=self.u.get_screen('home').ids.t1.text
+        self.b=Builder.load_string(kv)   
+        return self.b
     
-        self.r=requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=d122001753e3d91ae8aeb85e48687d80').json()                    
-        self.code=self.r['cod']
-          
-        try:              
-            sta=self.r['weather'][0]['main'].upper()
-            temp=str(int(self.r['main']['temp_max']-273.15)).upper()
-            
-            pre=str(self.r['main']['pressure'])
-            
-            ws=str(int(self.r['wind']['speed']*18/5))
-            des=self.r['weather'][0]['description'].upper()
-            
-            self.u.get_screen('home').ids.al1.text=sta
-            
-            self.u.get_screen('home').ids.al2.text=f'{temp}°C'
-            
-            self.u.get_screen('home').ids.al3.text=pre
-            
-            self.u.get_screen('home').ids.al4.text=f'{ws}km/h'
-            
-            self.u.get_screen('home').ids.al5.text=des
-            
-          
-                                              
-              
+    def add(self,te):
+        try:
+            self.b.get_screen('s1').ids.tf1.text+=te 
         except:
-            if self.code=='404':
-                toast('CITY NOT FOUND')
-                self.u.get_screen('home').ids.al1.text='---------------'
-                self.u.get_screen('home').ids.al2.text='---------------'
-                self.u.get_screen('home').ids.al3.text='---------------'
-                self.u.get_screen('home').ids.al4.text='---------------'
-                self.u.get_screen('home').ids.al5.text='---------------'
-                
-                
-            else:
-                toast('ERROR HAPPENED')    
-                self.u.get_screen('home').ids.al1.text='---------------'
-                self.u.get_screen('home').ids.al2.text='---------------'
-                self.u.get_screen('home').ids.al3.text='---------------'
-                self.u.get_screen('home').ids.al4.text='---------------'
-                self.u.get_screen('home').ids.al5.text='---------------'
-                 
-
-    def yv(self):
-        webbrowser.open('https://youtube.com/shorts/kKQv_wpl4vM?si=82XHYveNVQ4HFMmO')               
-                
-    def yc(self):
-        webbrowser.open('https://youtube.com/@ffgaming-su2eu?si=qofaL1xbplXI2Yx-')                                                       
-                                
-    def ap(self):
-        webbrowser.open("https://www.amazon.com/dp/B0DCVHKHSX/ref=apps_sf_sta")                                                    
-                                                                
-        
-
-Demo().run()
+            pass                                                      
+                                          
+    def solve(self):
+        so=self.b.get_screen('s1').ids.tf1.text 
+        try:
+            self.b.get_screen('s1').ids.tf1.text=str(eval(so))                                          
+        except:
+            toast('ERROR')
+            self.b.get_screen('s1').ids.tf1.text=''          
+    def remove(self):
+        try:
+            self.b.get_screen('s1').ids.tf1.text=''
+        except:
+            toast('SOORY,SOME ERROR PLEASE TRY AGAIN')            
+            
+    def bk(self):
+        try:
+            self.b.get_screen('s1').ids.tf1.do_backspace(from_undo=False,mode='bkspc')                        
+        except:
+            toast('SORRY,I TRYING TO REMOVE')                
+            
+    def thank(self):
+        try:
+            toast('THANKS,FOR USING OUR APP')  
+        except:
+            pass                                       
+ 
+                                           
+  
+                                                                                                      
+                                             
+            
+       
+de().run()     
